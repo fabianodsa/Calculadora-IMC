c4383ddb37df4d46961ba1871dba0a66b3928b4a
fabianodsa <fabyanoduarte@hotmail.com>
2022-12-04 23:19:42 -0300

    classificações do IMC vs condição criadas

--- main.js
+++ main.js
@@ -14,9 +14,26 @@ function imc () {
 
         const valorIMC = (peso / (altura * altura)).toFixed(1);
 
-        resultado.textContent = `${nome}, seu IMC é ${valorIMC}`;
+        let classificacao = '';
+        
+        if (valorIMC < 18.5) {
+            classificacao = "abaixo do peso";
+        } else if (valorIMC < 25) {
+            classificacao = "com o peso ideal.";
+        } else if (valorIMC < 30) {
+            classificacao = "levemente acima do peso.";
+        } else if (valorIMC < 35) {
+            classificacao = "com obesidade grau I.";
+        } else if (valorIMC < 40) {
+            classificacao = "com obesidade grau II.";
+        } else {
+            classificacao = 'com obesidade grau III';
+        }
+
+        resultado.textContent = `${nome}, seu IMC é ${valorIMC} e você está ${classificacao}`;
 
     } else {
         resultado.textContent= 'Preencha todos os campos';
     }
 }
+
